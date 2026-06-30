64896a61348c17b9f06fb1f9f2e3308e96bc8701
GDaniel19 <149326094+GDaniel19@users.noreply.github.com>
2026-06-30 18:21:58 -0500

    Add files via upload

---
+++ lib/format.ts
@@ -0,0 +1,19 @@
+export function formatCOP(value: number) {
+  return new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    maximumFractionDigits: 0
+  })
+    .format(value)
+    .replace('COP', '$')
+    .replace(/\s/g, '');
+}
+
+export function slugify(value: string) {
+  return value
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/(^-|-$)+/g, '');
+}
---
+++ lib/images.ts
@@ -0,0 +1,8 @@
+export const localMenuImages = [
+  '/images/menu/zero-burger.svg',
+  '/images/menu/classic-burger.svg',
+  '/images/menu/bacon-cheese.svg',
+  '/images/menu/loaded-fries.svg',
+  '/images/menu/hot-dog.svg',
+  '/images/menu/nachos.svg'
+];

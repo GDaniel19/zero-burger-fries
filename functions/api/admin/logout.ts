ad57f8f76f08fe89721dec74d9a40832484b1467
GDaniel19 <149326094+GDaniel19@users.noreply.github.com>
2026-07-01 11:31:02 -0500

    Delete functions directory

--- functions/api/admin/logout.ts
+++
@@ -1,6 +0,0 @@
-import { clearSessionCookie } from '../../_lib/auth';
-import { Env, json } from '../../_lib/responses';
-
-export const onRequestPost: PagesFunction<Env> = async ({ request }) => {
-  return json({ ok: true }, { headers: { 'set-cookie': clearSessionCookie(new URL(request.url).protocol === 'https:') } });
-};
